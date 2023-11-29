9a8428818b5d3a9b372b7ef86d4b77146b517e8e
rdevacc <rdevacc@gmail.com>
2023-11-29 10:49:17 +0700

    Change main routes

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [LoginController::class, 'index'])->middleware('guest')->name('login');
 
 Route::get('/app/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
